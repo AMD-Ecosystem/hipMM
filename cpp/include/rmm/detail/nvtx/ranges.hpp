49982708c85126ad43ba3d943d9296194da32659
vitor1001 <56533861+vitor1001@users.noreply.github.com>
2025-06-18 02:14:52 +0200

    Make RMM easier to BUILD with LLVM: (#1951)
    Make RMM easier to BUILD with LLVM:
    - add missing #includes;
    - do not require nvtx headers when its configured not to use nvtx.
    
    closes https://github.com/rapidsai/rmm/issues/1948 .
    
    Authors:
      - https://github.com/vitor1001
      - Bradley Dice (https://github.com/bdice)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Mark Harris (https://github.com/harrism)
    
    URL: https://github.com/rapidsai/rmm/pull/1951

--- cpp/include/rmm/detail/nvtx/ranges.hpp
+++ cpp/include/rmm/detail/nvtx/ranges.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, NVIDIA CORPORATION.
+ * Copyright (c) 2024-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,7 +82,6 @@ struct librmm_domain {
  * }
  * ```
  */
-#if defined(RMM_NVTX)
 #define RMM_FUNC_RANGE() NVTX3_FUNC_RANGE_IN(rmm::librmm_domain)
 #else
 #define RMM_FUNC_RANGE()
