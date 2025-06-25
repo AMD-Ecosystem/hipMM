d1bf56659c64fde35d56a698d0b48320799326d2
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-06-25 15:02:43 -0400

    refactor(shellcheck): enable for entire repo and fix all warnings (#1971)
    xref rapidsai/build-planning#135
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rmm/pull/1971

--- print_env.sh
+++ print_env.sh
@@ -63,17 +63,17 @@ echo
 
 echo "***Environment Variables***"
 
-printf '%-32s: %s\n' PATH $PATH
+printf '%-32s: %s\n' PATH "$PATH"
 
-printf '%-32s: %s\n' LD_LIBRARY_PATH $LD_LIBRARY_PATH
+printf '%-32s: %s\n' LD_LIBRARY_PATH "$LD_LIBRARY_PATH"
 
-# printf '%-32s: %s\n' NUMBAPRO_NVVM $NUMBAPRO_NVVM
+printf '%-32s: %s\n' NUMBAPRO_NVVM "$NUMBAPRO_NVVM"
 
-# printf '%-32s: %s\n' NUMBAPRO_LIBDEVICE $NUMBAPRO_LIBDEVICE
+printf '%-32s: %s\n' NUMBAPRO_LIBDEVICE "$NUMBAPRO_LIBDEVICE"
 
-printf '%-32s: %s\n' CONDA_PREFIX $CONDA_PREFIX
+printf '%-32s: %s\n' CONDA_PREFIX "$CONDA_PREFIX"
 
-printf '%-32s: %s\n' PYTHON_PATH $PYTHON_PATH
+printf '%-32s: %s\n' PYTHON_PATH "$PYTHON_PATH"
 
 echo
 
