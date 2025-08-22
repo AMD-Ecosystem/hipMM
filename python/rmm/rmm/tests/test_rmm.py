ed2d1767d0b6255d38b7a50c391e11f041d460c9
Bradley Dice <bdice@bradleydice.com>
2025-08-22 16:15:16 -0500

    Refactor device capability checks, fix prefetching bug on WSL (#2014)
    Refactors device capability checks, add concurrent managed access checks to skip prefetching tests on WSL (not supported).
    
    One motivation for this refactoring is that the previous names really didn't make much sense. We created the `runtime_async_alloc::is_supported()` to check for CUDA 11.2+ support for runtime async allocation. However, we tacked on other features like `is_export_handle_type_supported(cudaMemAllocationHandleType)` and `is_hwdecompress_supported()` into the same struct. Those should have been their own features, not tied to `runtime_async_alloc`. Most of the changes in this PR are structurally similar to that.
    
    Closes #2023.
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Tom Augspurger (https://github.com/TomAugspurger)
      - Matthew Murray (https://github.com/Matt711)
      - Mark Harris (https://github.com/harrism)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/rmm/pull/2014

--- python/rmm/rmm/tests/test_rmm.py
+++ python/rmm/rmm/tests/test_rmm.py
@@ -342,6 +342,17 @@ def test_rmm_device_buffer_pickle_roundtrip(hb):
     assert hb3 == hb
 
 
+def concurrent_managed_access_supported():
+    err, device_id = runtime.cudaGetDevice()
+    assert err == runtime.cudaError_t.cudaSuccess
+    err, supported = runtime.cudaDeviceGetAttribute(
+        runtime.cudaDeviceAttr.cudaDevAttrConcurrentManagedAccess,
+        device_id,
+    )
+    assert err == runtime.cudaError_t.cudaSuccess
+    return supported
+
+
 def assert_prefetched(buffer, device_id):
     import ctypes
     dev = ctypes.c_int(256)
@@ -363,13 +374,13 @@ def assert_prefetched(buffer, device_id):
 def test_rmm_device_buffer_prefetch(pool, managed):
     rmm.reinitialize(pool_allocator=pool, managed_memory=managed)
     db = rmm.DeviceBuffer.to_device(np.zeros(256, dtype="u1"))
-    if managed:
+    if managed and concurrent_managed_access_supported():
         assert_prefetched(db, runtime.cudaInvalidDeviceId)
     db.prefetch()  # just test that it doesn't throw
-    if managed:
-        err, device = runtime.cudaGetDevice()
+    if managed and concurrent_managed_access_supported():
+        err, device_id = runtime.cudaGetDevice()
         assert err == runtime.cudaError_t.cudaSuccess
-        assert_prefetched(db, device)
+        assert_prefetched(db, device_id)
 
 
 @pytest.mark.parametrize("stream", [cuda.default_stream(), cuda.stream()])
@@ -865,16 +876,14 @@ def test_prefetch_resource_adaptor(managed):
     # This allocation should be prefetched
     db = rmm.DeviceBuffer.to_device(np.zeros(256, dtype="u1"))
 
-    err, device = runtime.cudaGetDevice()
+    err, device_id = runtime.cudaGetDevice()
     assert err == runtime.cudaError_t.cudaSuccess
 
-    if managed:
-        assert_prefetched(db, device)
+    if managed and concurrent_managed_access_supported():
+        assert_prefetched(db, device_id)
     db.prefetch()  # just test that it doesn't throw
-    if managed:
-        err, device = runtime.cudaGetDevice()
-        assert err == runtime.cudaError_t.cudaSuccess
-        assert_prefetched(db, device)
+    if managed and concurrent_managed_access_supported():
+        assert_prefetched(db, device_id)
 
 
 def test_dev_buf_circle_ref_dealloc():
